9346ce85cb822cd37621d262c7706bbbc723a99f
原 夕香子 <HARAY1@tss.co.jp>
2022-10-28 16:12:24 +0900

    1028

--- tests/test_program.py
+++ tests/test_program.py
@@ -1,16 +1,16 @@
 from main.my_funcs import add, sub
 
-# def test_add():
-#     assert add(1, 2) == 3
+def test_add():
+    assert add(1, 2) == 2
 
 
-def test_sub():
-    assert sub(3, 1) == 2
+# def test_sub():
+#     assert sub(3, 1) == 2
 
 
-class TestCase:
-    def test_true(self):
-        assert True
+# class TestCase:
+#     def test_true(self):
+#         assert True
 
-    def test_add_zero(self):
-        assert add(1, 0) == 1
+    # def test_add_zero(self):
+    #     assert add(1, 0) == 1
